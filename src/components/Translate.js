b0d32d2185cfeff51647f018c1d3fb80a1c996f2
Ghate Suyog <suyogghateminks@gmail.com>
2022-11-07 19:45:26 +0530

    installed axios, calling the detect api using q as the header parameter

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import {
     Form,
     TextArea,
@@ -7,6 +8,27 @@ import {
 } from 'semantic-ui-react';
 
 export default function Translate() {
+    const [inputText, setInputText] = React.useState('');
+    const [detectLanguageKey, setdetectedLanguageKey] = React.useState('');
+    const [languageList, setLanguageList] = React.useState([]);
+
+    React.useEffect(() => {
+      axios.get('https://libretranslate.de/languages')
+      .then((response) => {
+        setLanguageList(response.data)
+      })
+    }, [])
+    
+
+    const getLanguageSource = () => {
+        axios.post('https://libretranslate.de/detect', {
+            q: inputText
+        })
+        .then((response) => {
+            setdetectedLanguageKey(response.data[0].language);
+        })
+    }
+
     return (
         <div>
             <div className="app-header">
@@ -16,12 +38,17 @@ export default function Translate() {
             <div className="app-body">
                 <div>
                     <Form>
-                        <Form.Field control={TextArea} placeholder='Type Text to Translate...'/>
+                        <Form.Field control={TextArea} placeholder='Type Text to Translate...' onChange={(e) => setInputText(e.target.value)}/>
                         <select className='language-select'>
                             <option>Please Select Language...</option>
+                            {languageList.map((language) => {
+                                return (
+                                    <option value={language.code}>{language.name}</option>
+                                )
+                            })}
                         </select>
                         <Form.Field control={TextArea} placeholder='Your Result Translation...'/>
-                        <Button color='orange' size='large'><Icon name='translate'/>Translate</Button>
+                        <Button color='orange' size='large' onClick={getLanguageSource}><Icon name='translate'/>Translate</Button>
                     </Form>
                 </div>
             </div>
